1eeb5cb0469236918f489a5f582f459e2ce5ce83
Brian Hewitt <sir.durendal@gmail.com>
2018-12-23 00:14:09 -0600

    Resolves #2, checks for thunk.default in applyMiddleware, if undefined passes thunk

--- example/src/index.jsx
+++ example/src/index.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { reducer as web3Redux } from 'web3-redux';
-import custom from './reducer';
-import App from './components/App';
+import React from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reducer as web3Redux } from "web3-redux";
+import custom from "./reducer";
+import App from "./components/App";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   combineReducers({ web3Redux, custom }),
-  composeEnhancers(applyMiddleware(thunk)),
+  composeEnhancers(applyMiddleware(thunk.default ? tunk.default : thunk))
 );
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
